042fe8dc1f796a8a479368d56fd5ff9b078edb63
Adam <relomy@gmail.com>
2019-09-09 23:07:51 -0400

    added contest_id to index template

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]", "192.168.1.109"]
 
 # Application definition
 INSTALLED_APPS = [
